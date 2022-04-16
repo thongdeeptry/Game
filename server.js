0b1d9108a2fe082537ec4c2f6ff1b4742619f8dc
Ngô Thành Thông <ngothanhthongcode@gmail.com>
2022-04-16 18:52:24 +0700

    a

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var io = socketIO(server,
 });
 var cuoc   = Array();
 var system = {
-	url : 'https://demo1.cardvip365.com', 
+	url : 'https://www.subfb78.online', 
 	keycode : '123456', 
 	ngocrong_min : 1000, 
 	ngocrong_max : 2000,
